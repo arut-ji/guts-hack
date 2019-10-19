b2cf2622c8468a85f5004bc463ffe8b5e38fbe32
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-10-19 18:06:26 +0100

    [Created] List view for destinations and journeys

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import BpkButton from 'bpk-component-button';
 import BpkText from 'bpk-component-text';
 import STYLES from './App.scss';
-import Home from "./pages/home";
+import Home from "./pages/Home";
+import LocationList from "./pages/LocationList/LocationListView"
 
 const c = className => STYLES[className] || 'UNKNOWN';
 
@@ -11,8 +12,8 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className={c('App')}>
-      <Home/>
+    <div>
+      <LocationList/>
     </div>
   );
 };
