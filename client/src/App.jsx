645abe605d02b09eb913186ade32733aec966dc6
arut klaewkasetkorn <60090002@kmitl.ac.th>
2019-10-19 12:22:25 +0100

    [Updated]: Change the reactt project to javascript.

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import BpkButton from 'bpk-component-button';
+import BpkText from 'bpk-component-text';
+import STYLES from './App.scss';
+import Home from "./pages/home";
+
+const c = className => STYLES[className] || 'UNKNOWN';
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={c('App')}>
+      <Home/>
+    </div>
+  );
+};
+
+export default App;
