5c58cb141e456e72e882833c4f997a1d6a91fdef
Pasuta Paopun <nrmfnnn.p@gmail.com>
2019-10-19 19:54:11 +0100

    Updated error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import BpkButton from 'bpk-component-button';
 import BpkText from 'bpk-component-text';
 import STYLES from './App.scss';
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 const c = className => STYLES[className] || 'UNKNOWN';
 
